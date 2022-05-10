5fe404ae5a13b18dcd7ee73a44169a45266dfaa4
Aybor <borisov_aleks@mail.ru>
2022-05-10 16:54:55 +0300

    написан проект парсера по ТЗ

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,5 +1,6 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
@@ -7,4 +8,22 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        all_peps = response.xpath('//*[@id="numerical-index"]//td[@class="num"]//a')
+        for pep_link in all_peps:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        number, name = response.xpath('//h1[@class="page-title"]/text()').get().split('–')
+        number = int(number.replace('PEP', ''))
+        name = name.strip()
+        status = response.xpath(
+            '//dl[@class="rfc2822 field-list simple"]'
+            '/dt[contains(text(), "Status")]'
+            '/following-sibling::dd[1]/text()'
+        ).get()
+        data = {
+            'number': number,
+            'name': name,
+            'status': status,
+        }
+        yield PepParseItem(data)
